92420867b57f718cac0c8b1bf721119ed4ffbb74
aouni <aounihalaweh@gmail.com>
2018-02-13 00:37:38 -0500

    UI

---
+++ YahtzeeActual/YahtzeeActual/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YahtzeeActual
+{
+    public class Dice
+    {
+        public Dice()
+        {
+        }
+
+        public void roll(TextBox text, CheckBox check)
+        {
+            if (check.Checked == false)
+            {
+                Random rand = new Random();
+                text.Text =  rand.Next().ToString();
+            }
+
+        }
+
+    }
+}
